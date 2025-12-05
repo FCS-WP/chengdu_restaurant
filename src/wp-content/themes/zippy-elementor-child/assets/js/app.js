751843f5c48d739018545c14eb3e186f6c0e6d18
nhanlt-FLS <nhan.letrong@floatingcube.com>
2025-12-05 17:54:24 +0700

    Create new page Chengdu Restaurant

--- src/wp-content/themes/zippy-elementor-child/assets/js/app.js
+++ src/wp-content/themes/zippy-elementor-child/assets/js/app.js
@@ -1,3 +1,37 @@
+import "../lib/slick/slick.js";
+
 $(document).ready(function ($) {
-  
+    $('.foods-slider').slick({
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 3,
+    arrows: true,
+    infinite: true,
+    adaptiveHeight: false,
+    useTransform: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"</button>',
+
+    responsive: [
+        {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+        }
+        }
+    ]
+    });
 });
+$('.foods-slider').slick('slickSetOption', 'slidesToScroll', 3, true);
